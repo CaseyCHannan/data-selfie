f44f1d4c43309a13a40539feae799faf2d77b7f5
Casey Hannan <Hannan.Casey@gmail.com>
2019-09-05 22:19:54 -0400

    simple server endpoint

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
-app.listen(3000, ()=> console.log('listening at 3000'))
+app.listen(3000, () => console.log('listening at 3000'))
 app.use(express.static('public'))
+app.post('/api', (request, response) => {
+    console.log(request)
+})
