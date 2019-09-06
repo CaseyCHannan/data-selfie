1128e0c1abc6179dcdfaa62fd41a0aa1bc25441a
Casey Hannan <Hannan.Casey@gmail.com>
2019-09-05 22:57:34 -0400

    returns json

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 app.listen(3000, () => console.log('listening at 3000'))
 app.use(express.static('public'))
+app.use(express.json({limit: '1mb'}))
 app.post('/api', (request, response) => {
-    console.log(request)
+    // console.log('I got a request')
+    console.log(request.body)
+    response.json({
+        status: 'success',
+        latitude: request.body.lat,
+        longitude: request.body.lon
+    })
 })
